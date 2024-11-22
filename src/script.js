6060689c98756259d5f9894553726572ddcd18bf
nishb369 <nishbcodes@gmail.com>
2024-11-22 22:51:01 +0530

    home page section created

---
+++ src/script.js
@@ -0,0 +1,7 @@
+var loadingPage = document.querySelector('.loading-page');
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        loadingPage.style.display = 'none';
+    }, 3000);
+});
+
