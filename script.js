14ba8e7fe27e3d002a34acbf2a1355719253d533
nishb369 <nishbcodes@gmail.com>
2024-11-24 22:05:11 +0530

    scanner page added

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var loadingPage = document.querySelector('.loading-page');
+var hoemPage = document.querySelector('.home-page');
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         loadingPage.style.display = 'none';
+        hoemPage.style.display = 'flex';
     }, 3000);
 });
 
