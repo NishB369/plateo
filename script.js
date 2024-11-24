427d8538cf05d0663a0f90c957cc348f149f7224
nishb369 <nishbcodes@gmail.com>
2024-11-24 22:08:59 +0530

    basic scanner page added

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ document.addEventListener("DOMContentLoaded", function() {
     }, 3000);
 });
 
+
+// Select the button element
+var scanBtn = document.querySelector("#nav-bar-bottom-center-btn"); // Make sure the class/ID matches your HTML structure
+
+// Add click event listener to the button
+scanBtn.addEventListener("click", function() {
+  // Navigate to scanner.html
+  window.location.href = "./scanner.html";
+});
